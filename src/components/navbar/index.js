c2382469db479fe4cf39d816c3cd5be58c5541b7
Meriem-bounaas <bouunaas.meriem.1993@gmail.com>
2022-10-08 22:32:20 +0200

    add active class style

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,22 +1,24 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { UilCheckCircle, UilClipboardNotes, UilStopwatch } from '@iconscout/react-unicons'
 import { useTranslation } from 'react-i18next';
 
 
 const Navbar = () => {
     const { t } = useTranslation();
+    let location = useLocation();
+    console.log(location.pathname);
 
-    return (
+    return ( 
         <nav className='flex flex-col gap-8 text-lg font-semibold text-primaryColor capitalize'>
-            <Link to="/" className='flex flex-row items-center gap-1 hover:text-thirdColor'>
+            <Link to="/" className={`flex flex-row items-center gap-1 hover:text-thirdColor  ${location.pathname==="/"?'text-second-color':'text-primary-color'}`}>
                 <UilClipboardNotes size="20" className="fill-second-color" />
                 {t("all tasks")}
             </Link>
-            <Link to="/completed" className='flex flex-row items-center gap-1 hover:text-thirdColor capitalize'>
+            <Link to="/completed" className={`flex flex-row items-center gap-1 hover:text-thirdColor ${location.pathname==="/completed"?'text-second-color':'text-primary-color'}`}>
                 <UilCheckCircle size="20" className="fill-second-color" />
                 {t("completed")}
             </Link>
-            <Link to="/inprogress" className='flex flex-row items-center gap-1 hover:text-thirdColor capitalize'>
+            <Link to="/inprogress" className={`flex flex-row items-center gap-1 hover:text-thirdColor ${location.pathname==="/inprogress"?'text-second-color':'text-primary-color'}`}>
                 <UilStopwatch size="20" className="fill-second-color" />
                 {t("in progress")}
             </Link>

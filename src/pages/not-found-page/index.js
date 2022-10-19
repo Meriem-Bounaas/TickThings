5a577a81994c9ee225d0d775b5e0fcb7f7b373cf
Meriem-bounaas <bouunaas.meriem.1993@gmail.com>
2022-10-19 22:56:36 +0200

    add page not found to router

---
+++ src/pages/not-found-page/index.js
@@ -0,0 +1,7 @@
+export default  function PageNotFound() {
+    return (
+      <div className="flex justify-center h-screen w-screen lg:mt-48 mt-20 ">
+        <h2 className="text-second-color font-bold text-2xl lg:text-5xl h-fit">404 Page not found</h2>
+      </div>
+    );
+  }
